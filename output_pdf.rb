dae3049ea24ae7ec9b62284d3b234375cba4a1bd
Masafumi Yokoyama <myokoym@gmail.com>
2014-01-02 17:35:00 +0900

    Fold a long line

--- output_pdf.rb
+++ output_pdf.rb
@@ -4,4 +4,7 @@ require 'fileutils'
 
 FileUtils.mkdir_p('pdf')
 
-system('rabbit --print --slides-per-page=2 --output-filename=pdf/chuork01-booklet.pdf')
+system('rabbit',
+       '--print',
+       '--slides-per-page=2',
+       '--output-filename=pdf/chuork01-booklet.pdf')
